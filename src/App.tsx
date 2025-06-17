a4e23430e00117d2b81e5e745741810cf2832cee
Devansh Bansal <devanshbansal500@gmail.com>
2025-06-18 00:42:22 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,10 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import { QueryClient, QueryClientProvider } from 'react-query'
-import { Toaster } from 'react-hot-toast'
-import Navbar from './components/layout/Navbar'
-import Footer from './components/layout/Footer'
-import Home from './pages/Home'
-import About from './pages/About'
-import Methodology from './pages/Methodology'
-import Results from './pages/Results'
 import Contact from './pages/Contact'
-import Dashboard from './pages/Dashboard'
-import Pricing from './pages/Pricing'
-
-const queryClient = new QueryClient()
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <div className="min-h-screen bg-gray-50">
-        <Navbar />
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/methodology" element={<Methodology />} />
-            <Route path="/results" element={<Results />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/pricing" element={<Pricing />} />
-          </Routes>
-        </main>
-        <Footer />
-        <Toaster 
-          position="top-right"
-          toastOptions={{
-            duration: 4000,
-            style: {
-              background: '#363636',
-              color: '#fff',
-            },
-          }}
-        />
-      </div>
-    </QueryClientProvider>
+    <div>
+      <Contact />
+    </div>
   )
 }
 
